126a54c7c5d62c16bafb7a7a2e131df46c67255e
Mohammad Raza <33965813+moeraza@users.noreply.github.com>
2021-02-13 20:53:45 -0500

    update to syntax

--- alig/tf/alig.py
+++ alig/tf/alig.py
@@ -68,7 +68,7 @@ class AliGwithoutMomentum(tf.compat.v1.train.GradientDescentOptimizer):
         self.eps = eps
 
     def minimize(self, loss, global_step=None, var_list=None,
-                 gate_gradients=tf.train.Optimizer.GATE_OP, aggregation_method=None,
+                 gate_gradients=tf.compat.v1.train.Optimizer.GATE_OP, aggregation_method=None,
                  colocate_gradients_with_ops=False, name=None,
                  grad_loss=None):
         return minimize(self, loss, global_step=global_step, var_list=var_list,
